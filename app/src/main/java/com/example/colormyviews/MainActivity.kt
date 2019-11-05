564508e4320545313caaa12610fbad5507d67403
HeHe27 <tanyg-wv17@student.tarc.edu.my>
2019-11-05 19:29:19 +0800

    Challenge

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -5,28 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import com.example.colormyviews.R.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
         setListeners()
     }
 
     private fun makeColored(view: View) {
         when (view.id) {
-
             // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+            id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            id.box_two_text -> view.setBackgroundResource(drawable.background_image)
+            id.box_three_text -> view.setBackgroundColor(Color.GRAY)
+            id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            id.box_six_text -> view.setBackgroundColor(Color.CYAN)
+            id.box_seven_text -> view.setBackgroundColor(Color.RED)
+            id.box_eight_text -> view.setBackgroundColor(Color.YELLOW)
+            id.red_button -> box_three_text.setBackgroundResource(color.my_red)
+            id.yellow_button -> box_four_text.setBackgroundResource(color.my_yellow)
+            id.green_button -> box_five_text.setBackgroundResource(color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
 
         }
@@ -34,21 +37,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
 
-        val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
-        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        val boxFourText = findViewById<TextView>(R.id.box_four_text)
-        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val boxOneText = findViewById<TextView>(id.box_one_text)
+        val boxTwoText = findViewById<TextView>(id.box_two_text)
+        val boxThreeText = findViewById<TextView>(id.box_three_text)
+        val boxFourText = findViewById<TextView>(id.box_four_text)
+        val boxFiveText = findViewById<TextView>(id.box_five_text)
+        val boxSixText = findViewById<TextView>(id.box_six_text)
+        val boxSevenText = findViewById<TextView>(id.box_seven_text)
+        val boxEightText = findViewById<TextView>(id.box_eight_text)
 
-        val redButton = findViewById<TextView>(R.id.red_button)
-        val greenButton = findViewById<TextView>(R.id.green_button)
-        val yellowButton = findViewById<TextView>(R.id.yellow_button)
+        val redButton = findViewById<TextView>(id.red_button)
+        val greenButton = findViewById<TextView>(id.green_button)
+        val yellowButton = findViewById<TextView>(id.yellow_button)
 
-        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        val rootConstraintLayout = findViewById<View>(id.constraint_layout)
 
         val clickableViews: List<View> =
             listOf(boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout,
+                boxFourText, boxFiveText,boxSixText
+                ,boxSevenText,boxEightText,rootConstraintLayout,
                 redButton, greenButton, yellowButton
             )
 
